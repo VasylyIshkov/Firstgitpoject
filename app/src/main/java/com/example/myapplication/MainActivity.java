2a3a8101a5de4d5e3aa5fafe49f443eb54834821
Xamarin <formyl17@gmail.com>
2020-07-05 11:09:05 +0300

    dev2, add Edittext

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+ EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
     }
     public void onClick(View v){
-        Toast.makeText(this,"OkClick",Toast.LENGTH_LONG).show();
+        String text = editText.getText().toString();
+        Toast.makeText(this,text,Toast.LENGTH_LONG).show();
     }
     public void onCansel(View v){
         Toast.makeText(this,"CanselClick",Toast.LENGTH_LONG).show();
+        editText.setText("");
     }
 }
