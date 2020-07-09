218bc77ff47dbc4ea610ac9c8d69338813403d66
Xamarin <formyl17@gmail.com>
2020-07-09 18:55:00 +0300

    Добавлена кнопка и поле ввода, обработчик кнопки, текст кнопки Send добавлен в ресурсы

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,23 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
  EditText editText;
+ Button send;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.editText);
+        editText = findViewById(R.id.EditText);
+        send = findViewById(R.id.Send);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendClick();
+            }
+        });
     }
-    public void onClick(View v){
-        String text = editText.getText().toString();
-        Toast.makeText(this,text,Toast.LENGTH_LONG).show();
+
+    private void SendClick(){
+        if(editText.getText().toString().length()>0){
+            Toast.makeText(this,editText.getText().toString(),Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this,"Введите текст!",Toast.LENGTH_LONG).show();
+        }
     }
-    public void onCansel(View v){
-        Toast.makeText(this,"CanselClick",Toast.LENGTH_LONG).show();
-        editText.setText("");
-    }
+
 }
